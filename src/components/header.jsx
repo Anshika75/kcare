03b84883adc6edd314692b3231b7e6048ea7b7b1
Anshika <anshikathakur596@gmail.com>
2024-04-17 14:35:30 +0530

    lorem impsum

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function header() {
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>header</div>
-  )
-}
+    <header className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <img src="/logo.svg" alt="Logo" className="h-8 mr-4" />
+          <nav className="hidden md:flex">
+            <a href="#" className="mx-2">About</a>
+            <a href="#" className="mx-2">Vision</a>
+            <a href="#" className="mx-2">Mission</a>
+            <a href="#" className="mx-2">Departments</a>
+            <a href="#" className="mx-2">Services</a>
+          </nav>
+        </div>
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="focus:outline-none">
+            <svg
+              className="h-6 w-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isMenuOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M19 12a1 1 0 0 1-1 1H6a1 1 0 0 1 0-2h12a1 1 0 0 1 1 1zm-7 5a1 1 0 1 1 0-2h6a1 1 0 1 1 0 2h-6a1 1 0 0 1 0 0zM5 8a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H6a1 1 0 0 1-1-1z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 6a1 1 0 0 1 1-1h14a1 1 0 0 1 0 2H5a1 1 0 0 1-1-1zm0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 0 2H5a1 1 0 0 1-1-1zm0 5a1 1 0 1 1 0-2h14a1 1 0 0 1 0 2H5z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+        <nav className={`md:flex ${isMenuOpen ? 'block' : 'hidden'}`}>
+          <a href="#" className="mx-2">About</a>
+          <a href="#" className="mx-2">Vision</a>
+          <a href="#" className="mx-2">Mission</a>
+          <a href="#" className="mx-2">Departments</a>
+          <a href="#" className="mx-2">Services</a>
+        </nav>
+        <div className="hidden md:flex items-center">
+          <a href="#" className="mx-2">Social Media</a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from '../components/header'
 
-export default function home() {
+export default function Home() {
   return (
-    <div>home</div>
+   <>
+    <div className="flex flex-col items-center justify-start w-full min-h-screen">
+      <Header/>
+    </div>
+   </>
   )
 }
