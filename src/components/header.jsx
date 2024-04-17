c178edb11c026358e4b9513448180470f9a6b843
Anshika <anshikathakur596@gmail.com>
2024-04-17 15:21:51 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,10 +8,10 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-[#fefefe] shadow-md w-full text-[#4396A2] poppins-medium p-4 lg:px-[50px]">
+    <header className="bg-[#fefefe] shadow-md w-full text-[#4396A2] poppins-medium p-5 lg:px-[50px]">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <div className="ml-0 text-2xl font-bold w-[15%]">
+        <div>
           <a href="/">
             <img
               src={logo}
@@ -20,12 +20,12 @@ const Header = () => {
             />
           </a>
         </div>
-        <div className="hidden lg:flex items-center">
+        <div className="hidden lg:flex items-center text-lg">
           <ul className="flex">
             <li>
               <a
                 href="#home"
-                className="p-4 mr-4 font-maven uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
+                className="p-3 mr-4 uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
               >
                 Home
               </a>
@@ -33,7 +33,7 @@ const Header = () => {
             <li>
               <a
                 href="#about"
-                className="p-4 mr-4 font-maven uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
+                className="p-3 mr-4 uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
               >
                 About
               </a>
@@ -41,7 +41,7 @@ const Header = () => {
             <li>
               <a
                 href="#sevents"
-                className="p-4 mr-4 font-maven uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
+                className="p-3 mr-4 uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
               >
                 Departments
               </a>
@@ -49,7 +49,7 @@ const Header = () => {
             <li>
               <a
                 href="#sachievements"
-                className="p-4 mr-4 font-maven uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
+                className="p-3 mr-4 uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
               >
                 Services
               </a>
@@ -57,7 +57,7 @@ const Header = () => {
             <li>
               <a
                 href="#partner"
-                className="p-4 mr-4 font-maven uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
+                className="p-3 mr-4 uppercase hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer"
               >
                 Contacts
               </a>
@@ -66,56 +66,75 @@ const Header = () => {
         </div>
 
         {/* Login button for laptop/desktop */}
-        <a href="https://drive.google.com/file/d/1HJYaFVWZCinxJSF3DI-cQnbwgagul3x8/view?usp=sharing">
-          <button className="w-[145px] justify-center font-bold hidden lg:block bg-[#E2012D] text-white text-center cursor-pointer transition-all duration-300 ease-in-out px-4 py-2 rounded hover:bg-white hover:text-red-600 rounded-tr-full">
-          Reimbursement
-          </button>
-        </a>
+        <div className="hidden lg:flex items-center justify-center text-2xl">
+          <a href="" className="grid place-items-center mx-2 transition-all hover:scale-90">
+            <i className="fa-brands fa-square-instagram"></i>
+          </a>
+          <a href="" className="grid place-items-center mx-2 transition-all hover:scale-90">
+            <i className="fa-brands fa-square-facebook"></i>
+          </a>
+          <a href="" className="grid place-items-center mx-2 transition-all hover:scale-90">
+            <i className="fa-brands fa-youtube"></i>
+          </a>
+          <a href="" className="grid place-items-center mx-2 transition-all hover:scale-90">
+            <i className="fa-solid fa-phone text-xl"></i>
+          </a>
+        </div>
 
         {/* Mobile navigation button */}
         <button className="lg:hidden" onClick={toggleMobileMenu}>
           {isMobileMenuOpen ? (
-            <p></p>
+            <i className="text-2xl fa-solid fa-xmark"></i>
           ) : (
-           <b></b>
+            <i className="text-2xl fa-solid fa-bars"></i>
           )}
         </button>
       </div>
 
       {/* Mobile navigation menu */}
       {isMobileMenuOpen && (
-        <div className="lg:hidden bg-black p-4 space-y-4 text-xl mt-3 hover: text-white hover:transition-opacity duration-500 ease-in-out border-b border-white hover:border-red-700">
-          <ul>
-            <a href="/">
-              <li className="font-maven uppercase p-4 mt-3 hover:text-red-600 hover:transition-opacity duration-500 ease-in-out border-b border-whit border-l-8 hover:border-red-700">
-                HOME
-              </li>
-            </a>
-            <a href="#about">
-              <li className="font-maven uppercase p-4 mt-3 hover:text-red-600 hover:transition-opacity duration-500 ease-in-out border-b border-white-600 border-l-8 hover:border-red-700">
-                ABOUT
-              </li>
-            </a>
-            <a href="#sevents">
-              <li className="font-maven uppercase p-4 mt-3 hover:text-red-600 hover:transition-opacity duration-500 ease-in-out border-b border-white-600 border-l-8 hover:border-red-700">
-                EVENTS
-              </li>
-            </a>
-            <a href="#sachievements">
-              <li className="font-maven uppercase p-4 mt-3 hover:text-red-600 hover:transition-opacity duration-500 ease-in-out border-b border-white-600 border-l-8 hover:border-red-700">
-                ACHIEVERS
-              </li>
-            </a>
-            <a href="#contact">
-              <li className="font-maven uppercase p-4 mt-3 hover:text-red-600 hover:transition-opacity duration-500 ease-in-out border-b border-white-600 border-l-8 hover:border-red-700">
-                CONTACT US
-              </li>
-            </a>
-            <a href="https://drive.google.com/file/d/1HJYaFVWZCinxJSF3DI-cQnbwgagul3x8/view?usp=sharing">
-            <button className="bg-[#E2012D] text-white py-2 lg:py-3 px-12 w-80 mt-12 flex justify-between items-center rounded-tr-full hover:bg-white hover:text-[#E2012D] transition-all duration-300 ease-in-out cursor-pointer font-oswald text-2xl lg:text-3xl uppercase tracking-widest">
-            REIMBURSEMENT &nbsp; &nbsp; X{" "}
-            </button>
-            </a>
+        <div className="lg:hidden py-4 space-y-4 text-xl mt-3 w-full text-[#4396A2]">
+        <ul className="w-full">
+            <li className="w-full hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer my-2 p-2">
+              <a
+                href="#home"
+                className="uppercase"
+              >
+                Home
+              </a>
+            </li>
+            <li className="w-full hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer my-2 p-2">
+              <a
+                href="#about"
+                className="uppercase"
+              >
+                About
+              </a>
+            </li>
+            <li className="w-full hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer my-2 p-2">
+              <a
+                href="#sevents"
+                className="uppercase"
+              >
+                Departments
+              </a>
+            </li>
+            <li className="w-full hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer my-2 p-2">
+              <a
+                href="#sachievements"
+                className="uppercase"
+              >
+                Services
+              </a>
+            </li>
+            <li className="w-full hover:bg-[#4396A2] hover:text-[#fefefe] transition-all cursor-pointer my-2 p-2">
+              <a
+                href="#partner"
+                className="uppercase"
+              >
+                Contacts
+              </a>
+            </li>
           </ul>
         </div>
       )}
