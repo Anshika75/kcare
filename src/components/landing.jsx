17680f782f78c0fb95f02589eb07b5d60963d074
Anshika <anshikathakur596@gmail.com>
2024-04-17 15:36:10 +0530

    Update landing.jsx

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,7 +4,7 @@ import home_banner from '../assets/home_banner.png'
 export default function Landing() {
   return (
     <>
-        <div className="grid place-items-center w-full mt-10 h-[35vh] lg:h-[100vh]">
+        <div className="grid place-items-center w-full mt-3 h-[35vh] lg:h-[100vh]">
             <img className='lg:object-cover w-full h-[35vh] lg:h-[100vh]' src={home_banner} alt="" />
         </div>
     </>
