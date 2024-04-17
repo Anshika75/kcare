56486bc86c9bd2876ae9d48366f4e9aac0cbde7a
Anshika <anshikathakur596@gmail.com>
2024-04-17 15:24:51 +0530

    Update landing.jsx

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import home_banner from '../assets/home_banner.png'
 
 export default function landing() {
   return (
     <>
         <div className="grid place-items-center w-full h-[95vh]">
-            <img src="" alt="" />
+            <img src={home_banner} alt="" />
         </div>
     </>
   )
