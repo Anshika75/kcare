73362a456a2b09ce747093cfe7dbcdd93d22ba4b
Anshika <anshikathakur596@gmail.com>
2024-04-17 14:43:26 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import Header from '../components/header'
 export default function Home() {
   return (
    <>
-    <div className="flex flex-col items-center justify-start w-full min-h-screen">
+    <div className="flex flex-col items-center justify-start w-full min-h-screen bg-[#4396A2]">
       <Header/>
     </div>
    </>
