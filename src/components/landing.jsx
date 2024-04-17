703ace65945036910bdc9fa7cd92bfec7da774fa
Anshika <anshikathakur596@gmail.com>
2024-04-17 15:23:07 +0530

    Create landing.jsx

---
+++ src/components/landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function landing() {
+  return (
+    <>
+        <div className="grid place-items-center w-full h-[95vh]">
+            <img src="" alt="" />
+        </div>
+    </>
+  )
+}
