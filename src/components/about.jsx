9f1cdf2b570f3be97a79eb3d207862b98b45ff25
Anshika <anshikathakur596@gmail.com>
2024-04-18 16:05:04 +0530

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export default function About() {
-  return (
-    <div>Aabout</div>
-  )
+    return (
+        <>
+            <div className="flex flex-col items-center">
+                <h1 className='text-[#4396A2] group-hover:text-[#fefefe] figtree-semibold text-3xl mt-6 md:mt-0 lg:text-5xl'>About Us+</h1>
+                <div className="flex justify-center poppins-normal items-center px-[25px] md:px-[50px]">
+
+                </div>
+            </div>
+        </>
+    )
 }
