f60a42f8c3451a561f6f200d8eee21afa98fb1b7
Anshika <anshikathakur596@gmail.com>
2024-04-18 15:49:38 +0530

    lorem

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -3,34 +3,34 @@ import React from 'react'
 export default function Stats() {
     return (
         <>
-            <div className="grid grid-cols-4 w-full px-[50px] pb-[50px] -mt-20 z-20">
-                <div className="box w-[300px] h-[150px] pt-[7%] bg-[#fff] shadow-md rounded-md relative flex flex-col items-center justify-center">
-                    <div className="absolute w-24 h-24 left-1/2 -translate-x-1/2 top-0 -translate-y-1/2 rounded-full bg-[#4396A2] grid place-items-center text-[#fefefe] text-4xl shadow-md">
+            <div className="grid grid-cols-1 gap-10 md:gap-16 md:grid-cols-2 lg:grid-cols-4 w-full px-[50px] pb-[50px] -mt-10 md:-mt-20 z-20">
+                <div className="box w-full h-[110px] lg:w-[300px] lg:h-[150px] lg:pt-[7%] bg-[#fff] shadow-md rounded-md relative flex flex-col items-center justify-center">
+                    <div className="absolute w-16 h-16 lg:w-24 lg:h-24 left-1/2 -translate-x-1/2 top-0 -translate-y-1/2 rounded-full bg-[#4396A2] grid place-items-center text-[#fefefe] text-3xl lg:text-4xl shadow-md">
                         <i className="fa-solid fa-building"></i>
                     </div>
-                    <h1 className='text-[#4396A2] figtree-semibold text-5xl'>150+</h1>
-                    <p className='text-[#333] popping-normal text-xl'>Happy Patients</p>
+                    <h1 className='text-[#4396A2] figtree-semibold text-3xl mt-6 md:mt-0 lg:text-5xl'>150+</h1>
+                    <p className='text-[#333] popping-normal -mt-1 text-lg lg:text-xl'>Happy Patients</p>
                 </div>
-                <div className="box w-[300px] h-[150px] pt-[7%] bg-[#fff] shadow-md rounded-md relative flex flex-col items-center justify-center">
-                    <div className="absolute w-24 h-24 left-1/2 -translate-x-1/2 top-0 -translate-y-1/2 rounded-full bg-[#4396A2] grid place-items-center text-[#fefefe] text-4xl shadow-md">
+                <div className="box w-full h-[110px] lg:w-[300px] lg:h-[150px] lg:pt-[7%] bg-[#fff] shadow-md rounded-md relative flex flex-col items-center justify-center">
+                    <div className="absolute w-16 h-16 lg:w-24 lg:h-24 left-1/2 -translate-x-1/2 top-0 -translate-y-1/2 rounded-full bg-[#4396A2] grid place-items-center text-[#fefefe] text-3xl lg:text-4xl shadow-md">
                         <i className="fa-solid fa-building"></i>
                     </div>
-                    <h1 className='text-[#4396A2] figtree-semibold text-5xl'>150+</h1>
-                    <p className='text-[#333] popping-normal text-xl'>Successful Surgeries</p>
+                    <h1 className='text-[#4396A2] figtree-semibold text-3xl mt-6 md:mt-0 lg:text-5xl'>150+</h1>
+                    <p className='text-[#333] popping-normal -mt-1 text-lg lg:text-xl'>Successful Surgeries</p>
                 </div>
-                <div className="box w-[300px] h-[150px] pt-[7%] bg-[#fff] shadow-md rounded-md relative flex flex-col items-center justify-center">
-                    <div className="absolute w-24 h-24 left-1/2 -translate-x-1/2 top-0 -translate-y-1/2 rounded-full bg-[#4396A2] grid place-items-center text-[#fefefe] text-4xl shadow-md">
+                <div className="box w-full h-[110px] lg:w-[300px] lg:h-[150px] lg:pt-[7%] bg-[#fff] shadow-md rounded-md relative flex flex-col items-center justify-center">
+                    <div className="absolute w-16 h-16 lg:w-24 lg:h-24 left-1/2 -translate-x-1/2 top-0 -translate-y-1/2 rounded-full bg-[#4396A2] grid place-items-center text-[#fefefe] text-3xl lg:text-4xl shadow-md">
                         <i className="fa-solid fa-building"></i>
                     </div>
-                    <h1 className='text-[#4396A2] figtree-semibold text-5xl'>150+</h1>
-                    <p className='text-[#333] popping-normal text-xl'>Furnished Rooms</p>
+                    <h1 className='text-[#4396A2] figtree-semibold text-3xl mt-6 md:mt-0 lg:text-5xl'>150+</h1>
+                    <p className='text-[#333] popping-normal -mt-1 text-lg lg:text-xl'>Furnished Rooms</p>
                 </div>
-                <div className="box w-[300px] h-[150px] pt-[7%] bg-[#fff] shadow-md rounded-md relative flex flex-col items-center justify-center">
-                    <div className="absolute w-24 h-24 left-1/2 -translate-x-1/2 top-0 -translate-y-1/2 rounded-full bg-[#4396A2] grid place-items-center text-[#fefefe] text-4xl shadow-md">
+                <div className="box w-full h-[110px] lg:w-[300px] lg:h-[150px] lg:pt-[7%] bg-[#fff] shadow-md rounded-md relative flex flex-col items-center justify-center">
+                    <div className="absolute w-16 h-16 lg:w-24 lg:h-24 left-1/2 -translate-x-1/2 top-0 -translate-y-1/2 rounded-full bg-[#4396A2] grid place-items-center text-[#fefefe] text-3xl lg:text-4xl shadow-md">
                         <i className="fa-solid fa-building"></i>
                     </div>
-                    <h1 className='text-[#4396A2] figtree-semibold text-5xl'>150+</h1>
-                    <p className='text-[#333] popping-normal text-xl'>Opertation Threaters</p>
+                    <h1 className='text-[#4396A2] figtree-semibold text-3xl mt-6 md:mt-0 lg:text-5xl'>150+</h1>
+                    <p className='text-[#333] popping-normal -mt-1 text-lg lg:text-xl'>Opertation Threaters</p>
                 </div>
             </div>
         </>
