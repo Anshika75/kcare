8fc9d8c7ea0040f2f1589f7035ec355545b313c4
Anshika <anshikathakur596@gmail.com>
2024-04-18 15:12:03 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/header'
 import Landing from '../components/landing'
+import Stats from '../components/stats'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-start w-full min-h-screen bg-[#4396A2]">
       <Header/>
       <Landing/>
+      <Stats/>
     </div>
    </>
   )
