6184ff1115a822f32f56c30cf8a2b19f32e86bb2
Anshika <anshikathakur596@gmail.com>
2024-04-20 12:11:25 +0530

    lorem

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function About() {
     return (
         <>
-            <div className="flex flex-col items-center">
-                <h1 className='text-[#4396A2] group-hover:text-[#fefefe] figtree-semibold text-3xl mt-6 md:mt-0 lg:text-5xl'>About Us+</h1>
-                <div className="flex justify-center poppins-normal items-center px-[25px] md:px-[50px]">
+            <div className="flex flex-col items-center px-[25px] md:px-[50px] my-10">
+                <h1 className='text-[#4396A2] group-hover:text-[#fefefe] figtree-semibold text-3xl mt-6 md:mt-0 lg:text-7xl'>About Us</h1>
+                <div className="flex justify-center poppins-normal items-center">
 
                 </div>
             </div>
