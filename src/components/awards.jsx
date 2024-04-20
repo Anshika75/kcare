3173688ecbc285b410dad493b96687191fa1d1db
Anshika <anshikathakur596@gmail.com>
2024-04-20 22:26:10 +0530

    Create awards.jsx

---
+++ src/components/awards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function awards() {
+  return (
+    <>
+        <div className="flex flex-col w-full bg-[#4396A2] px-[25px] md:px-[50px] md:my-10"></div>
+    </>
+  )
+}
