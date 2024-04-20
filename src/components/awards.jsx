4b1699deba72f8465160b214ca9af86e97bdfd89
Anshika <anshikathakur596@gmail.com>
2024-04-20 22:26:23 +0530

    Update awards.jsx

--- src/components/awards.jsx
+++ src/components/awards.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function awards() {
+export default function Awards() {
   return (
     <>
         <div className="flex flex-col w-full bg-[#4396A2] px-[25px] md:px-[50px] md:my-10"></div>
