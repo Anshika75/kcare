8637e5a64c30982da8367a55b85be94de79bb1db
Anshika <anshikathakur596@gmail.com>
2024-04-22 01:07:00 +0530

    Update departments.jsx

--- src/components/departments.jsx
+++ src/components/departments.jsx
@@ -88,7 +88,7 @@ const departments = [
 export default function Departments() {
     return (
         <>
-            <div className="flex flex-col items-center relative w-full md:px-[50px] overflow-hidden px-[25px] md:pb-20 md:my-10 bg-[#f9f9f9]">
+            <div id="dept" className="flex flex-col items-center relative w-full md:px-[50px] overflow-hidden px-[25px] md:pb-20 md:my-10 bg-[#f9f9f9]">
                 <h1 className='text-[#4396A2] figtree-semibold text-4xl md:mt-0 md:text-7xl mb-3 md:mb-6 relative after:absolute after:bottom-0 after:left-0 after:h-1 after:w-12 lg:after:w-24 after:bg-[#4396A2]'>Departments</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-4 md:gap-8 md:gap-y-12 lg:gap-y-20 lg:grid-cols-4 mt-6 md:mt-16 z-20">
                     {departments.map((department, index) => (
