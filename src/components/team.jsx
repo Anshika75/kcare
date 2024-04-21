b1a592dc188a56fc735e002fe27cfd331b400dca
Anshika <anshikathakur596@gmail.com>
2024-04-21 22:25:46 +0530

    Update team.jsx

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -1,7 +1,83 @@
 import React from 'react'
 
 export default function Team() {
-  return (
-    <div>team</div>
-  )
+    return (
+        <div className="flex flex-col items-center w-full md:px-[50px] md:pb-[50px] px-[25px] pb-[25px] md:my-10">
+            <h1 className='text-[#4396A2] figtree-semibold text-4xl mt-6 md:mt-0 md:text-7xl mb-6 relative after:absolute after:bottom-0 after:left-0 after:h-1 after:w-16 lg:after:w-32 after:bg-[#4396A2]'>Our Team</h1>
+            <div className="grid gird-cols-1 md:grid-cols-2 gap-4 md:gap-16 lg:grid-cols-4 w-full mt-6">
+                <div className="box relative w-full overflow-hidden rounded-md shadow-md transition-all hover:scale-105 cursor-pointer">
+                    <img className='object-cover w-full h-[400px]' src="https://static.vecteezy.com/system/resources/thumbnails/028/287/384/small_2x/a-mature-indian-male-doctor-on-a-white-background-ai-generated-photo.jpg" alt="" />
+                    <div className="absolute bottom-0 left-0flex flex-col p-6 bg-[#4396A2] w-full">
+                        <h1 className='text-[#fefefe] figtree-semibold text-2xl'>Dr. John Doe</h1>
+                        <p className='text-[#fefefe] poppins-normal text-lg'>Cardiologist</p>
+                        <div className="social absolute right-0 top-0 bg-[#fefefe]  h-full px-4 flex flex-col items-center justify-evenly">
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-solid fa-phone  text-[#4396A2] text-lg"></i>
+                            </a>
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-brands fa-square-instagram text-[#4396A2] text-xl"></i>
+                            </a>
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-brands fa-square-facebook text-[#4396A2] text-xl"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="box relative w-full overflow-hidden rounded-md shadow-md transition-all hover:scale-105 cursor-pointer">
+                    <img className='object-cover w-full h-[400px]' src="https://static.vecteezy.com/system/resources/thumbnails/028/287/384/small_2x/a-mature-indian-male-doctor-on-a-white-background-ai-generated-photo.jpg" alt="" />
+                    <div className="absolute bottom-0 left-0flex flex-col p-6 bg-[#4396A2] w-full">
+                        <h1 className='text-[#fefefe] figtree-semibold text-2xl'>Dr. John Doe</h1>
+                        <p className='text-[#fefefe] poppins-normal text-lg'>Cardiologist</p>
+                        <div className="social absolute right-0 top-0 bg-[#fefefe]  h-full px-4 flex flex-col items-center justify-evenly">
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-solid fa-phone  text-[#4396A2] text-lg"></i>
+                            </a>
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-brands fa-square-instagram text-[#4396A2] text-xl"></i>
+                            </a>
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-brands fa-square-facebook text-[#4396A2] text-xl"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="box relative w-full overflow-hidden rounded-md shadow-md transition-all hover:scale-105 cursor-pointer">
+                    <img className='object-cover w-full h-[400px]' src="https://static.vecteezy.com/system/resources/thumbnails/028/287/384/small_2x/a-mature-indian-male-doctor-on-a-white-background-ai-generated-photo.jpg" alt="" />
+                    <div className="absolute bottom-0 left-0flex flex-col p-6 bg-[#4396A2] w-full">
+                        <h1 className='text-[#fefefe] figtree-semibold text-2xl'>Dr. John Doe</h1>
+                        <p className='text-[#fefefe] poppins-normal text-lg'>Cardiologist</p>
+                        <div className="social absolute right-0 top-0 bg-[#fefefe]  h-full px-4 flex flex-col items-center justify-evenly">
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-solid fa-phone  text-[#4396A2] text-lg"></i>
+                            </a>
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-brands fa-square-instagram text-[#4396A2] text-xl"></i>
+                            </a>
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-brands fa-square-facebook text-[#4396A2] text-xl"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="box relative w-full overflow-hidden rounded-md shadow-md transition-all hover:scale-105 cursor-pointer">
+                    <img className='object-cover w-full h-[400px]' src="https://static.vecteezy.com/system/resources/thumbnails/028/287/384/small_2x/a-mature-indian-male-doctor-on-a-white-background-ai-generated-photo.jpg" alt="" />
+                    <div className="absolute bottom-0 left-0flex flex-col p-6 bg-[#4396A2] w-full">
+                        <h1 className='text-[#fefefe] figtree-semibold text-2xl'>Dr. John Doe</h1>
+                        <p className='text-[#fefefe] poppins-normal text-lg'>Cardiologist</p>
+                        <div className="social absolute right-0 top-0 bg-[#fefefe]  h-full px-4 flex flex-col items-center justify-evenly">
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-solid fa-phone  text-[#4396A2] text-lg"></i>
+                            </a>
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-brands fa-square-instagram text-[#4396A2] text-xl"></i>
+                            </a>
+                            <a href="">
+                                <i className="transition-all hover:text-[#C4EBE9] fa-brands fa-square-facebook text-[#4396A2] text-xl"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
