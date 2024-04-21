d9fc8bbf8e0b6b2c3bfa0b5cc7d0cbe621221c75
Anshika <anshikathakur596@gmail.com>
2024-04-21 22:35:33 +0530

    Update testimonial.jsx

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -10,73 +10,113 @@ import 'swiper/css/pagination';
 import './testimonial.css';
 
 // import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Autoplay,  } from 'swiper/modules';
 
 export default function Testimonial() {
     return (
         <>
-            <div className="flex flex-col items-center w-full md:px-[50px] md:pb-[50px] px-[25px] pb-[25px] md:my-10">
-            <h1 className='text-[#4396A2] figtree-semibold text-4xl mt-6 md:mt-0 md:text-7xl mb-6 relative after:absolute after:bottom-0 after:left-0 after:h-1 after:w-16 lg:after:w-32 after:bg-[#4396A2]'>Testimonials</h1>
-            <Swiper
-                loop={true}
-                loopFillGroupWithBlank={true}
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={'auto'}
-                pagination={true}
-                modules={[EffectCoverflow, Pagination]}
-                className="mySwiper tswiper mb-20"
-                coverflowEffect={{
-                    rotate: 10,
-                    stretch: 50,
-                    depth: 200,
-                    modifier: 1,
-                    slideShadows: true,
-                }}
-                breakpoints={{
-                    640: {
-                        slidesPerView: 1,
-                        spaceBetween: 20,
-                    },
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 30,
-                    },
-                    1024: {
-                        slidesPerView: 3,
-                        spaceBetween: 150,
-                    },
-                }}
-            >
-                <SwiperSlide className='tswiper-slide'>
-                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='tswiper-slide'>
-                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='tswiper-slide'>
-                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='tswiper-slide'>
-                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='tswiper-slide'>
-                    <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='tswiper-slide'>
-                    <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='tswiper-slide'>
-                    <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='tswiper-slide'>
-                    <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='tswiper-slide'>
-                    <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-                </SwiperSlide>
-            </Swiper>
+            <div className="flex flex-col items-center relative w-full md:px-[50px] overflow-hidden px-[25px] py-4 md:pb-20 md:py-12 my-10 bg-[#4396a2]">
+                <h1 className='text-[#fefefe] figtree-semibold text-4xl mt-6 md:mt-0 md:text-7xl mb-6 relative after:absolute after:bottom-0 after:left-0 after:h-1 after:w-16 lg:after:w-32 after:bg-[#fefefe]'>Testimonials</h1>
+                <Swiper
+                    loop={true}
+                    loopFillGroupWithBlank={true}
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    pagination={true}
+                    modules={[Autoplay, EffectCoverflow, Pagination]}
+                    className="mySwiper tswiper"
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    coverflowEffect={{
+                        rotate: 10,
+                        stretch: 50,
+                        depth: 200,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 1,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 30,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 150,
+                        },
+                    }}
+                >
+                    <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-6">
+                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-6">
+                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-6">
+                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-6">
+                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-6">
+                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-6">
+                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-6">
+                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-6">
+                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-6">
+                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </>
     );
