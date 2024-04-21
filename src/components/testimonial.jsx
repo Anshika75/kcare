b7407c9acc4dee07bf0ab540bba6a4879c07c3d2
Anshika <anshikathakur596@gmail.com>
2024-04-21 21:33:24 +0530

    lorem

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import './testimonial.css';
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 export default function Testimonial() {
-  return (
-    <div>testimonial</div>
-  )
+    return (
+        <>
+            <Swiper
+                loop={true}
+                loopFillGroupWithBlank={true}
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination]}
+                className="mySwiper tswiper mb-20"
+                coverflowEffect={{
+                    rotate: 10,
+                    stretch: 50,
+                    depth: 200,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                breakpoints={{
+                    640: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 30,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 150,
+                    },
+                }}
+            >
+                <SwiperSlide className='tswiper-slide'>
+                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className='tswiper-slide'>
+                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className='tswiper-slide'>
+                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className='tswiper-slide'>
+                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className='tswiper-slide'>
+                    <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className='tswiper-slide'>
+                    <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className='tswiper-slide'>
+                    <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className='tswiper-slide'>
+                    <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className='tswiper-slide'>
+                    <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                </SwiperSlide>
+            </Swiper>
+        </>
+    );
 }
