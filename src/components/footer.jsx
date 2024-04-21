70d316723ee7b77665ad277ed3b861443040af90
Anshika <anshikathakur596@gmail.com>
2024-04-22 00:40:27 +0530

    lorem

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 import footerlogo from "../assets/logo.png";
-
+import services from "../assets/services.png";
 
 const Footer = () => {
   return (
-    <div id={"contact"} className="relative w-full">
+    <div id={"contact"} className="relative w-full lg:mt-[7%]">
       <div className="flex flex-col lg:flex-row z-50 absolute w-[90%] lg:w-[55%] bg-[#E2012D] py-3 font-extrabold font-maven text-white -translate-y-3/4 left-[50%] -translate-x-1/2 min-h-[120px] justify-between lg:px-12 lg:items-center px-2">
         <div className="">
-          <p className="text-3xl font-oswald">PASSION IN ACTION</p>
-          <p className="text-3xl font-oswald mt-3 text-[#091022]">DISCOVER OUR DYNAMIC TEAM</p>
+          <p className="text-3xl figtree-bold uppercase">Launching Soon</p>
+          <p className="text-3xl figtree-bold uppercase mt-1 text-[#091022]">free health checkup camps</p>
         </div>
         <div>
-          <a to="/team"
-            type="submit"
-            className="bg-[#091022] flex items-center justify-evenly text-white py-2 mt-3 rounded-tr-full w-[150px] hover:bg-[#121f2e]"
-          >
-            View Team &gt;
-          </a>
+         <img src={services} alt="" />
         </div>
       </div>
-      <div className="bg-[#091022] lg:mt-[12%]">
+      <div className="bg-[#091022]">
         <div className="w-full px-3 mx-auto mt-60 lg:mt-0 py-12 pt-24 flex flex-col lg:flex-row gap-4 text-gray-300">
           <div className="w-full lg:pl-8 lg:w-[60%]">
             <h1 className="w-full text-3xl text-[#E2012D]">
