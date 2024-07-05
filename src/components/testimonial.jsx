d00e760530112ead93855ab27dbdb85a430f3b0a
anshika75 <anshikathakur596@gmail.com>
2024-07-05 21:50:30 +0530

    Update testimonial.jsx

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -15,7 +15,7 @@ import { EffectCoverflow, Pagination, Autoplay,  } from 'swiper/modules';
 export default function Testimonial() {
     return (
         <>
-            <div className="flex flex-col items-center relative w-full md:px-[50px] overflow-hidden px-[25px] py-4 md:pb-20 md:py-12 my-10 bg-[#4396a2]">
+            <div className="flex flex-col items-center relative w-full md:px-[50px] overflow-hidden px-[25px] py-4 md:pb-20 md:py-12 my-10 bg-[#0D5094]">
                 <h1 className='text-[#fefefe] figtree-semibold text-4xl mt-6 md:mt-0 md:text-7xl mb-6 relative after:absolute after:bottom-0 after:left-0 after:h-1 after:w-16 lg:after:w-32 after:bg-[#fefefe]'>Testimonials</h1>
                 <Swiper
                     loop={true}
@@ -54,65 +54,65 @@ export default function Testimonial() {
                 >
                     <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
                         <div className="flex flex-col items-center justify-center w-full h-full p-6">
-                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
-                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
-                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                            <p className='text-[#333] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#0D5094] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#DE2225] poppins-normal text-lg'>Cardiologist</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
                         <div className="flex flex-col items-center justify-center w-full h-full p-6">
-                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
-                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
-                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                            <p className='text-[#333] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#0D5094] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#DE2225] poppins-normal text-lg'>Cardiologist</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
                         <div className="flex flex-col items-center justify-center w-full h-full p-6">
-                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
-                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
-                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                            <p className='text-[#333] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#0D5094] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#DE2225] poppins-normal text-lg'>Cardiologist</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
                         <div className="flex flex-col items-center justify-center w-full h-full p-6">
-                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
-                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
-                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                            <p className='text-[#333] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#0D5094] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#DE2225] poppins-normal text-lg'>Cardiologist</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
                         <div className="flex flex-col items-center justify-center w-full h-full p-6">
-                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
-                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
-                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                            <p className='text-[#333] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#0D5094] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#DE2225] poppins-normal text-lg'>Cardiologist</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
                         <div className="flex flex-col items-center justify-center w-full h-full p-6">
-                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
-                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
-                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                            <p className='text-[#333] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#0D5094] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#DE2225] poppins-normal text-lg'>Cardiologist</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
                         <div className="flex flex-col items-center justify-center w-full h-full p-6">
-                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
-                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
-                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                            <p className='text-[#333] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#0D5094] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#DE2225] poppins-normal text-lg'>Cardiologist</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
                         <div className="flex flex-col items-center justify-center w-full h-full p-6">
-                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
-                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
-                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                            <p className='text-[#333] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#0D5094] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#DE2225] poppins-normal text-lg'>Cardiologist</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='tswiper-slide bg-[#fefefe]'>
                         <div className="flex flex-col items-center justify-center w-full h-full p-6">
-                            <p className='text-[#4396A2] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
-                            <h1 className='text-[#4396A2] figtree-semibold text-2xl mt-4'>John Doe</h1>
-                            <p className='text-[#4396A2] poppins-normal text-lg'>Cardiologist</p>
+                            <p className='text-[#333] poppins-normal text-lg lg:text-center text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed etiam, ut inquit, est ita: Si id quod dicis, inquit, est, tibi istud placet. Quae cum dixisset paulumque institisset, Quid est? </p>
+                            <h1 className='text-[#0D5094] figtree-semibold text-2xl mt-4'>John Doe</h1>
+                            <p className='text-[#DE2225] poppins-normal text-lg'>Cardiologist</p>
                         </div>
                     </SwiperSlide>
                 </Swiper>
